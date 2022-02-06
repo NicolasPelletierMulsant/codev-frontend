2d4e910f26164f251cb59b3525fda61ea6414f16
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-07 00:29:00 +0100

    Added AvatarMenu

---
+++ components/AvatarMenu.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+const AvatarMenuOptions = [
+    { text: "Profile", link: "#" },
+    { text: "Se déconnecter", link: "#" },
+];
+
+export default function AvatarMenu(props) {
+    const [anchorElAvatar, setAnchorElAvatar] = React.useState(null);
+
+    const handleOpenAvatarMenu = (event) => { setAnchorElAvatar(event.currentTarget); };
+    const handleCloseAvatarMenu = (event) => { setAnchorElAvatar(null); };
+
+    return (
+        <Box>
+            <IconButton onClick={handleOpenAvatarMenu}>
+                <Avatar src="./img/avatar_test.jpg" />
+            </IconButton>
+            <Menu anchorEl={anchorElAvatar} open={Boolean(anchorElAvatar)} onClose={handleCloseAvatarMenu}>
+                {AvatarMenuOptions.map((option) => {
+                    return (
+                        <MenuItem key={option.text}>
+                            <Typography textAlign="center">
+                                <Link href={option.link} sx={{ textDecoration: "none" }} variant="inherit">
+                                    {option.text}
+                                </Link>
+                            </Typography>
+                        </MenuItem>
+                    );
+                })}
+            </Menu>
+        </Box>
+    );
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,34 @@
+import AvatarMenu from './AvatarMenu.jsx';
+
 import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+const isConnected = true;
 
 export default function Navbar(props) {
   return (
-    <AppBar sx={{ backgroundColor: "gray" }}>
-      <Toolbar>
-      <a href="#">
-        <Box sx={{
-            display: "flex", 
-            justifyContent: "center", 
-            alignItems: "center"
-          }}>
-          <img src="ousama.png" alt="logo" width="70px" height="70px"/>
-          <span>Test</span>
-        </Box>
-      </a>
-
+    <AppBar>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Link href="#" sx={{ textDecoration: "none", color: "white" }}>
+          <Box sx={{
+              display: "flex", 
+              alignItems: "center",
+            }}>
+            <img src="./img/ousama.png" alt="logo" width="70px" height="70px"/>
+            <Typography variant="h6" sx={{ marginLeft: "10px" }}>Projet Codev</Typography>
+          </Box>
+        </Link>
+        
+        {isConnected
+          ? <AvatarMenu>Test</AvatarMenu>
+          : <Button>Se connecter</Button>
+        }
+        
       </Toolbar>
     </AppBar>
   )
