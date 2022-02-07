804d8bb858ca09da772a0f928e64e75ef60564a6
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-07 21:27:17 +0100

    Moved Home page link to sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 import AvatarMenu from './AvatarMenu.jsx';
 
+import * as React from 'react';
 import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
@@ -12,17 +13,10 @@ const isConnected = true;
 
 export default function Navbar(props) {
   return (
-    <AppBar>
+    <AppBar position="fixed" sx={ props.sx }>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Link href="#" sx={{ textDecoration: "none", color: "white" }}>
-          <Box sx={{
-              display: "flex", 
-              alignItems: "center",
-            }}>
-            <img src="./img/ousama.png" alt="logo" width="70px" height="70px"/>
-            <Typography variant="h6" sx={{ marginLeft: "10px" }}>Projet Codev</Typography>
-          </Box>
-        </Link>
+        
+        <Typography variant="h6" noWrap component="div">{ props.pageName }</Typography>
         
         {isConnected
           ? <AvatarMenu>Test</AvatarMenu>
