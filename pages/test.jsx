1c2b5f7a6413f5f3c03818905c7f2fad5a96fbd7
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-07 21:27:53 +0100

    Created test page (to test Routing)

---
+++ pages/test.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout.jsx'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+    return (
+        <Layout pageName="Test">
+            <Typography>Test</Typography>
+        </Layout>
+    );
+}
