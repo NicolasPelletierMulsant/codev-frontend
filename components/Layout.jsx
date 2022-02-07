cfac258a22eaae8d6dffaa114608318a95f3bd29
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-07 21:26:45 +0100

    Added Layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Box from '@mui/material/Box'
+import Navbar from "../components/Navbar.jsx"
+import { Sidebar, sidebarWidth } from '../components/Sidebar.jsx'
+import Typography from '@mui/material/Typography'
+
+export default function Layout(props) {
+    return (
+        <Box display={{ display: "flex" }}>
+            <Head>
+                <title>Projet Codev</title>
+                <link rel="icon" href="./img/site_logo.png" />
+            </Head>
+
+            <Navbar pageName={props.pageName} sx={{ width: `calc(100% - ${sidebarWidth}px)`, ml: `${sidebarWidth}px` }}>
+            </Navbar>
+
+            <Sidebar>
+            </Sidebar>
+
+            <Box component="main" sx={{ flexGrow: 1, paddingTop: "5rem", paddingLeft: "1rem" }}>
+                {props.children}
+            </Box>
+        </Box>
+    );
+}
