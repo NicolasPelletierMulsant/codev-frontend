53ce970cf6b38513229e855be274df0fc8a8be8a
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-09 15:07:53 +0100

    Added leaflet dependencies

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,6 +10,12 @@ export default function Layout(props) {
             <Head>
                 <title>Projet Codev</title>
                 <link rel="icon" href="./img/site_logo.png" />
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+                    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+                    crossorigin=""/>
+                <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+                    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+                    crossorigin=""></script>
             </Head>
 
             <Navbar pageName={props.pageName} sx={{ width: `calc(100% - ${sidebarWidth}px)`, ml: `${sidebarWidth}px` }}>
