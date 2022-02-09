0b8585f91af395355c1cb2a5e3980998ba70e5ef
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-09 20:30:40 +0100

    Refactored profile

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -10,44 +10,6 @@ import TextField from '@mui/material/TextField';
 const username = "JohnDoe";
 const email = "test@gmail.com";
 
-const card = (
-    <React.Fragment>
-        <CardContent>
-            <Box
-                component="form"
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                    '& *': { m: 1 },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <Box sx={{ display: "flex", justifyContent: "center" }}>
-                    <Avatar src="./img/avatar_test.jpg" sx={{ width: "80px", height: "80px" }} />
-                </Box>
-                <TextField
-                    className="outlined-read-only-input"
-                    label="Nom d'utilisateur"
-                    defaultValue={username}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-                <TextField
-                    className="outlined-read-only-input"
-                    label="Adresse email"
-                    defaultValue={email}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-            </Box>
-        </CardContent>
-    </React.Fragment>
-)
-
 export default function Home() {
     return (
         <Layout pageName="Profile">
@@ -64,7 +26,39 @@ export default function Home() {
                     alignItems: "center" ,
                     minWidth: "18rem",
                 }}>
-                {card}
+                    <CardContent>
+                        <Box
+                            component="form"
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                '& *': { m: 1 },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <Box sx={{ display: "flex", justifyContent: "center" }}>
+                                <Avatar src="./img/avatar_test.jpg" sx={{ width: "80px", height: "80px" }} />
+                            </Box>
+                            <TextField
+                                className="outlined-read-only-input"
+                                label="Nom d'utilisateur"
+                                defaultValue={username}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                            />
+                            <TextField
+                                className="outlined-read-only-input"
+                                label="Adresse email"
+                                defaultValue={email}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                            />
+                        </Box>
+                    </CardContent>
                 </Card>
             </Box>
         </Layout>
