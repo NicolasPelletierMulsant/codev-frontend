73db85c0aa97de1d51170f6edc85d85871e18676
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-09 15:20:44 +0100

    Created Map component

---
+++ pages/map.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import dynamic from 'next/dynamic'
+import Layout from '../components/Layout.jsx'
+import Box from '@mui/material/Box';
+
+export default function Home() {
+
+    // Dynamic import to use client side rendering
+    const Map = dynamic(
+        () => import('../components/Map.jsx'),
+        { ssr: false }
+    );
+
+    return (
+        <Layout pageName="Carte">
+            <Box sx={{
+                display: "flex",
+                flexGrow: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%"
+            }}>
+                <Map />
+            </Box>
+        </Layout>
+    );
+}
