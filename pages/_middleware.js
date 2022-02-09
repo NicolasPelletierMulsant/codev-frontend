e7fbbbbf33e8c615c65e4f06fcc1a5a6a4f289ba
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-02-09 11:55:14 +0100

    Created register page

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,8 +4,12 @@ export default function middleware(req, ev) {
     const url = req.nextUrl.clone();
     const cookies = req.cookies;
 
-    if (!cookies.connected && url.pathname !== "/login") {
-        return NextResponse.rewrite(url.origin + "/login");
+    if (!cookies.connected && (url.pathname !== "/login" || url.pathname !== "/register")) {
+        if (url.pathname === "/register") {
+            return NextResponse.rewrite(url.origin + "/register")
+        } else {
+            return NextResponse.rewrite(url.origin + "/login");
+        }
     }
-
+    return NextResponse.rewrite(url);
 };
---
+++ pages/register.jsx
@@ -0,0 +1,156 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Router from 'next/router';
+import Cookies from 'universal-cookie';
+
+const theme = createTheme();
+
+export default function Home() {
+  
+  const emailRegex = /^.+@\w+\.\w+$/g
+
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = React.useState(false);
+  const [emailValid, setEmailValid] = React.useState(true);
+  
+  const handleEmailChange = (event) => {
+    if (!event.currentTarget.value.match(emailRegex)) {
+      setEmailValid(false);
+    } else {
+      setEmailValid(true);
+    }
+  }
+
+  React.useEffect(() => {
+    if (password !== confirmPassword) {
+      setConfirmPasswordError(true);
+    } else {
+      setConfirmPasswordError(false);
+    }
+  }, [password, confirmPassword]);
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.currentTarget.value);
+  }
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const email = data.get("email");
+    const username = data.get("username");
+    const password = data.get("password");
+    const confirmPassword = data.get("confirm-password");
+
+    if (!(email && username && password && confirmPassword)) {
+        alert("Veuillez remplir tous les champs");
+        return;
+    }
+
+    if (!email.match(emailRegex)) {
+      alert("Format d'email invalide");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Les mots de passe ne correspondent pas");
+        return;
+    }
+
+    // Handle registration
+    const cookies = new Cookies();
+    cookies.set("connected", true);
+    Router.push('/');
+  };
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Créer un compte
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Adresse email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleEmailChange}
+              error={!emailValid}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Nom d'utilisateur"
+              name="username"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Mot de passe"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handlePasswordChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="confirm-password"
+              label="Confirmer mot de passe"
+              type="password"
+              id="confirm-password"
+              autoComplete="current-password"
+              onChange={handleConfirmPasswordChange}
+              error={confirmPasswordError}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Confirmer
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
